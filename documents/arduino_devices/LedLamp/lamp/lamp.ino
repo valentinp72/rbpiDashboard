e1816335e3ca9b2a850a925fd4aac4295e3750dd
Valentin Pelloin <valentinpelloin@gmail.com>
2017-12-23 22:29:28 +0100

    led strip code

--- documents/arduino_devices/LedLamp/lamp/lamp.ino
+++ documents/arduino_devices/LedLamp/lamp/lamp.ino
@@ -1,14 +1,58 @@
-#include <RCSwitch.h>
+#include <RCSwitch.h>           // https://github.com/sui77/rc-switch
+#include <PinChangeInterrupt.h> // https://github.com/NicoHood/PinChangeInterrupt
+
+#include <EEPROM.h>             // https://www.arduino.cc/en/Reference/EEPROM
+
+
+#define PIN_RECEPTEUR 2 // pin utilisé pour le recepteur : doit être un pin interruptible
+#define PIN_RUBAN     3 // pin du transistor du ruban
+#define PIN_BUTTON    4 // le pin utilisé pour le bouton pour forcer le toggle
+
+#define TEMPS_STEP  5   // temps entre chaque pas du changement de la luminosité, en ms
+#define ADRESSE_MEM 0   // l'adresse en memoire EEPROM de l'état du ruban
 
 RCSwitch mySwitch = RCSwitch();
 
+float currentState = 0.0;
+
+
 void setup() {
   Serial.begin(9600);
-  mySwitch.enableReceive(0);  // Receiver on inerrupt 0 => that is pin #2
-  Serial.println("We are ready");
+  
+  mySwitch.enableReceive(digitalPinToInterrupt(PIN_RECEPTEUR));
+  
+  pinMode(PIN_RUBAN,  OUTPUT);
+  pinMode(PIN_BUTTON, INPUT_PULLUP);
+
+  // utilisation de la librairie PinChangeInterrupt pour l'interruption
+  // par le bouton : il aurait simplement fallu souder le bouton sur un
+  // pin interruptible ...
+  // https://github.com/NicoHood/PinChangeInterrupt
+  attachPCINT(digitalPinToPCINT(PIN_BUTTON), interruptButtonForce, RISING);
+  
+  EEPROM.get(ADRESSE_MEM, currentState);
+  if(currentState < 0 || currentState > 255)
+    currentState = 0;
+  analogWrite(PIN_RUBAN, currentState);
+  
+  TCCR1B = TCCR1B & 0b11111000 | 0x02;
+}
+
+void interruptButtonForce() {
+  if(currentState == 0)
+    currentState = 255;
+  else
+    currentState = 0;
+    
+  analogWrite(PIN_RUBAN, currentState);
+  EEPROM.put(ADRESSE_MEM, currentState);
+
+  // reset to main loop
+  asm volatile ("  jmp 0"); 
 }
 
 void loop() {
+
   if (mySwitch.available()) {
     
     int value = mySwitch.getReceivedValue();
@@ -17,21 +61,54 @@ void loop() {
       Serial.print("Unknown encoding");
     } else {
       
-      char code[50];
-      itoa(mySwitch.getReceivedValue(), code, 10);
-      Serial.println(code);
+      String code = String(mySwitch.getReceivedValue());
+      
+      if(code[0] == '1' && code.length() == 6){
+        int toState  = code.substring(1, 4).toInt();
+        int timeToGo = code.substring(4, 6).toInt();
 
-      if(code[0] == '1' && strlen(code) == 6){
-        Serial.println("It's for me!");
+        if(toState >= 0 && toState <= 255 && timeToGo >= 0 && timeToGo <= 99) {
+
+          Serial.println("To do : ");
+          Serial.print("Actual: ");
+          Serial.println(currentState);
+          Serial.print("Want: ");
+          Serial.println(toState);
+          Serial.print("In seconds: ");
+          Serial.println(timeToGo);            
+
+          int i;
+          int steps = toState - currentState;
+          int maxSteps = timeToGo * (1000 / TEMPS_STEP);
+          float stepValue = steps / (maxSteps * 1.0);
+          
+          for(i = 0 ; i < maxSteps ; i++) {
+            currentState += stepValue;
+            analogWrite(PIN_RUBAN, currentState);
+            delay(TEMPS_STEP);
+          }
+
+          currentState = toState;
+          analogWrite(PIN_RUBAN, currentState);
+          EEPROM.put(ADRESSE_MEM, currentState);
+
+          Serial.println("");
+
+        }
+        else {
+          Serial.println("Invalide state / time");
+        }
+
       }
       else {
         Serial.println("Not for me");
       }
-       Serial.println(strlen(code));
-       Serial.println(code[0]);
 
     }
 
     mySwitch.resetAvailable();
   }
+  delay(100);
 }
+
+
